8cfc1a13f265cc50b4da4bdec7bc595bc38b0d1c
Rodrigo Taveira <rodrigotaveiira@gmail.com>
2026-08-14 21:31:18 -0300

    Corrige caminho da foto da Maria Clara Kemp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -82,7 +82,7 @@ const testimonials = [
 Estar no saberes tornou o processo, que não é fácil, muito mais leve! O ambiente é super acolhedor nos mínimos detalhes! 
 Minha gratidão eterna a esse lugar e essa profissional maravilhosa. 
 É difícil achar alguém como Carlinha hoje em dia`,
-    image: 'h/maria-clara-kemp.jpeg',
+    image: '/maria-clara-kemp.jpeg',
   },
   {
     name: 'Thaís Taveres',
